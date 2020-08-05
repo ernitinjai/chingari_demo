c326c53cecba4dab30fc41110724c93c52d16711
nitin <ernitinjai@gmail.com>
2020-08-05 15:59:33 +0530

    Asked for location run time permission

--- app/src/main/java/com/chingari/chingariweatherdemo/MainActivity.kt
+++ app/src/main/java/com/chingari/chingariweatherdemo/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.chingari.chingariweatherdemo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.chingari.chingariweatherdemo.util.Constants
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupPermissions()
     }
+
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+                Constants.LOCATION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            Constants.LOCATION_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    //Permission not granted, lets provide details to user why we need it.
+                    showAssistMessage()
+                }
+            }
+        }
+    }
+
+    fun showAssistMessage() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.location_ask_title)
+        builder.setMessage(R.string.location_ask_message)
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton(getString(R.string.location_ask_button_label)){dialogInterface, which ->
+            setupPermissions()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
+
+
+
 }
---
+++ app/src/main/java/com/chingari/chingariweatherdemo/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.chingari.chingariweatherdemo.util
+
+
+
+class Constants {
+    companion object {
+        val LOCATION_REQUEST_CODE = 101
+    }
+}
