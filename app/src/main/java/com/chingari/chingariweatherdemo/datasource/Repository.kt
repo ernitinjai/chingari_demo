7617b07e3e2fcb0fc0be72546f5aa251ebf23308
nitin <ernitinjai@gmail.com>
2020-08-06 06:56:01 +0530

    Created repository

---
+++ app/src/main/java/com/chingari/chingariweatherdemo/datasource/Repository.kt
@@ -0,0 +1,44 @@
+package com.chingari.chingariweatherdemo.datasource
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.chingari.chingariweatherdemo.datasource.local.WeatherModel
+import com.chingari.chingariweatherdemo.datasource.local.WeatherDataBase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class Repository {
+
+    companion object {
+
+        var weatherDataBase: WeatherDataBase? = null
+
+        var getWeatherData: List<WeatherModel>? = null
+
+        fun initializeDB(context: Context) : WeatherDataBase {
+            return WeatherDataBase.invoke(context)
+        }
+
+        fun insertWeatherData(context: Context, temperature: String, humidity: String, windspeed:String) {
+
+            weatherDataBase = initializeDB(context)
+
+            CoroutineScope(IO).launch {
+                val weather = WeatherModel(temperature, humidity,windspeed)
+                weatherDataBase!!.weatherDao().insertAll(weather)
+            }
+
+        }
+
+        fun getWeatherData(context: Context) : LiveData<List<WeatherModel>>? {
+
+            weatherDataBase = initializeDB(context)
+
+            getWeatherData = weatherDataBase!!.weatherDao().getAll()
+
+            return getWeatherData as LiveData<List<WeatherModel>>
+        }
+
+    }
+}
---
+++ app/src/main/java/com/chingari/chingariweatherdemo/datasource/local/WeatherModel.kt
@@ -0,0 +1,24 @@
+package com.chingari.chingariweatherdemo.datasource.local
+
+import android.text.TextUtils
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.chingari.chingariweatherdemo.model.WeatherResponse
+
+@Entity(tableName = "weather_items")
+data class GetWeatherModel(
+
+
+        @ColumnInfo(name = "temperature")
+        var temperature: String,
+        @ColumnInfo(name = "humidity")
+        var humidity: String,
+        @ColumnInfo(name = "windspeed")
+        var windspeed: String
+    ){
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Int?=null
+}
+
